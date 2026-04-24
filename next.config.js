cd3883f87e1a637b9a671caf10e41fbf56093114
Leadgen247 <chady@leadgen247.com>
2026-04-23 23:20:34 -0400

    Add rewrites for legal pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
+
+Copy
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
-      { source: '/', destination: '/index.html' },
+      { source: '/disclaimer', destination: '/disclaimer.html' },
+      { source: '/terms', destination: '/terms.html' },
+      { source: '/privacy', destination: '/privacy.html' },
+      { source: '/cookies', destination: '/cookies.html' },
+      { source: '/acceptable-use', destination: '/acceptable-use.html' },
+      { source: '/support', destination: '/support.html' },
     ];
   },
 };
-
+ 
 module.exports = nextConfig;
