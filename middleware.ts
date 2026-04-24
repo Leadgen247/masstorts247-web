0f052d7efb1bc029eb8cd0aaf17a324ec7de66c6
Leadgen247 <chady@leadgen247.com>
2026-04-23 22:32:17 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -14,9 +14,10 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   runtime: 'nodejs',
   matcher: [
-    // Skip Next internals and static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/dashboard/:path*',
+    '/dashboard',
+    '/api/user/:path*',
+    '/signin/:path*',
+    '/sign-up/:path*',
   ],
 };
